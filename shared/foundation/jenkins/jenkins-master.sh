79f32d67bd1a86948d99a648e7a08b8b202909ff
AntNeo <antneo.love@gmail.com>
2024-08-20 07:10:31 +0800

    change jenkins ec2 type

--- shared/foundation/jenkins/jenkins-master.sh
+++ shared/foundation/jenkins/jenkins-master.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+set -e
 sudo apt-get update
 sudo apt-get install -y docker.io
 sudo systemctl start docker
@@ -12,7 +14,7 @@ sudo mkdir /var/jenkins_home && sudo chown ubuntu:ubuntu /var/jenkins_home
 sudo docker run -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home --restart=always --name jenkins jenkins/jenkins:lts
 
 # wait master up
-sleep 10
+sleep 30
 
 # Output Jenkins initial admin password
 INITIAL_PASSWORD=$(sudo docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
@@ -20,7 +22,11 @@ echo $INITIAL_PASSWORD
 
 # download jenkins-cli.jar
 sudo docker exec jenkins curl -s http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/jenkins-cli.jar
-
+if [ $? -ne 0 ]; then
+    echo "Failed to download jenkins-cli.jar"
+    exit 1
+fi
+echo "====================================================================="
 # create a new admin user
 sudo docker exec -i jenkins sh -c "echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${jenkins_admin}\", \"${jenkins_password}\")' | java -jar /var/jenkins_home/jenkins-cli.jar -auth admin:$INITIAL_PASSWORD -s http://localhost:8080/ groovy ="
 
